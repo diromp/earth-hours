62dbef9d067e175d2a131ed4f2103dca593662fb
dirom purbowiseno <dirom@catalyzecommunications.com>
2024-01-10 06:31:44 +0700

    fix carousel

--- public/js/main.js
+++ public/js/main.js
@@ -4,21 +4,45 @@ $(document).ready(function () {
     var languageJp = $('body').hasClass('language-jp');
     var languageCs = $('body').hasClass('language-cs');
     var languageBm = $('body').hasClass('language-bm');
+
     var $window = $(window);
     var $itemSaveDSlide = $('.item-card-saved'),
         index2 = 0,
-        visible2 = 3,
+        visible2 = 6,
         endIndexSaved = ($itemSaveDSlide.length / visible2) - 1;
-
     var $item = $('.item-card'), //Cache your DOM selector
         visible = 4, //Set the number of items that will be visible
         index = 0, //Starting index
-        endIndex = ($item.length / visible) - 1;
+    endIndex = ($item.length / visible) - 1;
+    var slideDiscoveryContainer = $('#slide-discovery');
+    var sliderSavedContainer = $('#savedSlider');
+    var heightSlide = $(slideDiscoveryContainer).height();
+    var heightSlide2 = $(sliderSavedContainer).height()/2;
+    if ($window.width() < 800) {
+        var splitItemMobile = $("#slide-discovery .item-card-container .item-card");
+        var splitItemMobileSaved = $("#savedSlider .item-card-container .item-card-saved");
 
-    var heightSlide = $('.new-card').height() / 4;
-    var heightSlide2 = $('.new-card').height() / 3;
+        for (var i = 0; i < splitItemMobile.length; i += 4) {
+            splitItemMobile.slice(i, i + 4).wrapAll(`<div class="container-card"></div>`);
+        }
+        for (var i = 0; i < splitItemMobileSaved.length; i += 4) {
+            splitItemMobileSaved.slice(i, i + 4).wrapAll(`<div class="container-card-saved"></div>`);
+        }
+    }
+
+    var $itemMobile = $('.container-card'),
+        $lastIndexMobile = $itemMobile.length - 2;
+    var $itemMobileSaved = $('.container-card-saved'),
+        $lastIndexMobileSaved = $itemMobileSaved.length - 2;
+    var $itemCardContainer = $('#slide-discovery .item-card-container');
+    var $itemCardContainerSaved = $('#savedSlider .item-card-container');
 
 
+    if (index == 0 || index2 == 0) {
+        $('.slick-prev').addClass('slick-disabled');
+        $('.slick-prev-saved').addClass('slick-disabled');
+    }
+    // click save favorite button
     $('.event').on('click', function (e) {
         $('.slide-1').fadeOut(
             'slow'
@@ -26,9 +50,12 @@ $(document).ready(function () {
         $('.saved-list').fadeIn(
             'slow'
         );
-        $itemSaveDSlide.animate({ 'top': '0px' })
+        $('.slick-prev-saved').addClass('slick-disabled');
+        $itemCardContainerSaved.animate({ 'top': '0px' })
         index2 = 0;
     });
+    
+    // click back to home button
     $('.close-saved').on('click', function () {
         $('.saved-list').fadeOut(
             'slow'
@@ -36,92 +63,162 @@ $(document).ready(function () {
         $('.slide-1').fadeIn(
             'slow'
         );
-        $item.animate({ 'top': '0px' })
+        $itemCardContainer.animate({ 'top': '0px' })
+        $('.slick-prev').addClass('slick-disabled');
         index = 0;
     });
 
+    // slider for Discovery action mobile and desktop
     if ($window.width() > 1080) {
         $('.slick-next').on("click", function () {
-            console.log(index);
-            console.log(endIndexSaved);
+            let calEndIndex = Math.ceil(endIndex) - 1;
             if (index < endIndex) {
+                if (index == 0) {
+                    $('.slick-prev').removeClass('slick-disabled');
+                } else if (index == calEndIndex) {
+                    $('.slick-next').addClass('slick-disabled');
+                } else {
+                    $('.slick-next').removeClass('slick-disabled');
+                }
+
                 index++;
-                $item.animate({
-                    'top': `-=${heightSlide}`
-                });
+                var calculate =  animateSlide(true, index, 0, heightSlide);
+                console.log(calculate);
+                
+                $itemCardContainer.animate({
+                    'top': `-=${heightSlide}px`
+                }, 500);
             }
         });
 
         $('.slick-prev').on("click", function () {
+            if (index > 0) {
+                if (index == 1) {
+                    $('.slick-prev').addClass('slick-disabled');
+                    $('.slick-next').removeClass('slick-disabled');
+                } else {
+                    $('.slick-next').removeClass('slick-disabled');
+                }
+                index--;
+                $itemCardContainer.animate({
+                    'top': `+=${heightSlide}px`
+                }, 500);
+            }
+        });
+
+    } else {
+        $('.slick-next').on("click", function () {
+            var checkPosition = $itemMobile.width() + 10;
+            if (index < endIndex) {
+                if (index == 0) {
+                    $('.slick-prev').removeClass('slick-disabled');
+                } else if (index == $lastIndexMobile) {
+                    $('.slick-next').addClass('slick-disabled');
+                } else {
+                    $('.slick-next').removeClass('slick-disabled');
+                }
+                index++;
+                $itemCardContainer.animate({
+                    'left': `-=${checkPosition}`,
+                }, 500);
+            }
+        });
+
+        $('.slick-prev').on("click", function () {
+            var checkPosition = $itemMobile.width() + 10;
 
             if (index > 0) {
+                if (index == 1) {
+                    $('.slick-prev').addClass('slick-disabled');
+                    $('.slick-next').removeClass('slick-disabled');
+                }
                 index--;
-                $item.animate({
-                    'top': `+=${heightSlide}`
-                });
+                
+                $itemCardContainer.animate({
+                    'left': `+=${checkPosition}`,
+                }, 500);
+            }
+        });
+    }
+
+    // slider for SavedSlider action mobile and desktop
+
+    if ($window.width() > 1080) {
+        $('.slick-next-saved').on("click", function () {
+            let calEndIndex = Math.ceil(endIndexSaved) - 1;
+            console.log(heightSlide2)
+            if (index2 < endIndexSaved) {
+                if (index2 == 0) {
+                    $('.slick-prev-saved').removeClass('slick-disabled');
+                } else if (index2 == calEndIndex) {
+                    $('.slick-next-saved').addClass('slick-disabled');
+                } else {
+                    $('.slick-next-saved').removeClass('slick-disabled');
+                }
+
+                index2++;
+                $itemCardContainerSaved.animate({
+                    'top': `-=${heightSlide2}`
+                }, 500);
+            }
+        });
+
+        $('.slick-prev-saved').on("click", function () {
+
+            if (index2 > 0)
+                if (index2 == 1) {
+                    $('.slick-prev-saved').addClass('slick-disabled');
+                    $('.slick-next-saved').removeClass('slick-disabled');
+                } else {
+                    $('.slick-next-saved').removeClass('slick-disabled');
+                } {
+                index2--;
+                $itemCardContainerSaved.animate({
+                    'top': `+=${heightSlide2}`
+                }, 500);
             }
         });
     } else {
-        $('.slick-next').on("click", function () {
+        $('.slick-next-saved').on("click", function () {
+            var checkPosition = $itemCardContainerSaved.width()* ($itemMobileSaved.length - 1);
+
+            console.log(checkPosition)
+            console.log($itemMobileSaved.length - 1);
+
             if (index < endIndex) {
+                if (index == 0) {
+                    $('.slick-prev-saved').removeClass('slick-disabled');
+                } else if (index == $lastIndexMobileSaved) {
+                    $('.slick-next-saved').addClass('slick-disabled');
+                } else {
+                    $('.slick-next-saved').removeClass('slick-disabled');
+                }
                 index++;
-                $item.animate({
-                    'top': '-=337px'
-                });
+
+                $itemCardContainerSaved.animate({
+                    'left': `-=${checkPosition}`,
+                }, 500);
             }
         });
 
-        $('.slick-prev').on("click", function () {
+        $('.slick-prev-saved').on("click", function () {
+            var checkPosition = $itemMobileSaved.width();
 
             if (index > 0) {
+                if (index == 1) {
+                    $('.slick-prev-saved').addClass('slick-disabled');
+                    $('.slick-next-saved').removeClass('slick-disabled');
+                }
                 index--;
-                $item.animate({
-                    'top': '+=337px'
-                });
-            }
-        });
-    }
-
-    if ($window.width() > 1080) {
-        $('.slick-next-saved').on("click", function () {
-            if (index2 < endIndexSaved) {
-                index2++;
-                $itemSaveDSlide.animate({
-                    'top': `-=${heightSlide2}`
-                });
-            }
-        });
-
-        $('.slick-prev-saved').on("click", function () {
-            if (index2 > 0) {
-                index2--;
-                $itemSaveDSlide.animate({
-                    'top': `+=${heightSlide2}`
-                });
-            }
-        });
-    }
-    else {
-        $('.slick-next-saved').on("click", function () {
-            if (index2 < endIndexSaved) {
-                index2++;
-                $itemSaveDSlide.animate({
-                    'top': '-=330px'
-                });
-            }
-        });
-
-        $('.slick-prev-saved').on("click", function () {
-            if (index2 > 0) {
-                index2--;
-                $itemSaveDSlide.animate({
-                    'top': '+=330px'
-                });
+                $itemCardContainerSaved.animate({
+                    'left': `+=${checkPosition}`,
+                }, 500);
             }
         });
     }
 
     $('#reset-filter').hide();
+    
     $('.item-card .love').click(function () {
         var activeClass = $(this).hasClass('active');
         $(this).toggleClass('active');
@@ -441,41 +538,45 @@ $(document).ready(function () {
     });
 });
 
+
+
+// scroll on browser
+
 const slider = document.querySelector('.list');
 let isDown = false;
 let startX;
 let scrollLeft;
 
 slider.addEventListener('mousedown', (e) => {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-  cancelMomentumTracking();
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+    cancelMomentumTracking();
 });
 
 
 slider.addEventListener('mouseleave', () => {
-  isDown = false;
-  slider.classList.remove('active');
+    isDown = false;
+    slider.classList.remove('active');
 });
 
 
 slider.addEventListener('mouseup', () => {
-  isDown = false;
-  slider.classList.remove('active');
-  beginMomentumTracking();
+    isDown = false;
+    slider.classList.remove('active');
+    beginMomentumTracking();
 });
 
 
 slider.addEventListener('mousemove', (e) => {
-  if(!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 3; //scroll-fast
-  var prevScrollLeft = slider.scrollLeft;
-  slider.scrollLeft = scrollLeft - walk;
-  velX = slider.scrollLeft - prevScrollLeft;
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3; //scroll-fast
+    var prevScrollLeft = slider.scrollLeft;
+    slider.scrollLeft = scrollLeft - walk;
+    velX = slider.scrollLeft - prevScrollLeft;
 });
 
 // Momentum 
@@ -484,21 +585,21 @@ var velX = 0;
 var momentumID;
 
 slider.addEventListener('wheel', (e) => {
-  cancelMomentumTracking();
-});  
+    cancelMomentumTracking();
+});
 
-function beginMomentumTracking(){
-  cancelMomentumTracking();
-  momentumID = requestAnimationFrame(momentumLoop);
-}
-function cancelMomentumTracking(){
-  cancelAnimationFrame(momentumID);
-}
-function momentumLoop(){
-  slider.scrollLeft += velX;
-  velX *= 0.95; 
-  if (Math.abs(velX) > 0.5){
+function beginMomentumTracking() {
+    cancelMomentumTracking();
     momentumID = requestAnimationFrame(momentumLoop);
-  }
+}
+function cancelMomentumTracking() {
+    cancelAnimationFrame(momentumID);
+}
+function momentumLoop() {
+    slider.scrollLeft += velX;
+    velX *= 0.95;
+    if (Math.abs(velX) > 0.5) {
+        momentumID = requestAnimationFrame(momentumLoop);
+    }
 }
 
