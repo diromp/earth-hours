f684410043c3f09e9fe5ba6f7dbe5f63bdc46dc8
Andry Muharyo <andrymuharyo@gmail.com>
2024-01-12 15:00:27 +0800

    update

--- public/js/slides.js
+++ public/js/slides.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
             }
         ]
     });
-    
+
     $('.event').on('click', function (e) {
         $('.slide-1').fadeOut(
             'slow'
@@ -33,7 +33,7 @@ $(document).ready(function () {
 
     // click back to home button
     $('.close-saved').on('click', function () {
-        //slider.slick('setPosition'); 
+        //slider.slick('setPosition');
         $('.saved-list').fadeOut(
             'slow'
         );
@@ -42,22 +42,65 @@ $(document).ready(function () {
         );
     });
 
+    $sliderDiscovery.on('destroy', function(event, slick, currentSlide, nextSlide){
+        console.log(event);
+    });
+
     $('.close').on('click', function (e) {
         $('.collapse-menu').toggleClass('collapsed');
         $('.content-dis').toggleClass('collapsed');
         $('.filter').toggleClass('!hidden');
         $(this).toggleClass('active');
 
-        console.log('muask')
-        $sliderDiscovery.slick('slickSetOption', 'rows', 3);
+        //$sliderDiscovery.slick('slickSetOption', 'rows', 3);
+        $sliderDiscovery.slick('destroy');
+
         if ($('.content-dis').hasClass('collapsed')) {
-            
+            $sliderDiscovery.slick({
+                dots: false,
+                infinite: false,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                rows: 3,
+                draggable: false,
+                arrows: true,
+                autoplay: false,
+                vertical: true,
+                appendArrows: $(this).find('.arrows'),
+                responsive: [
+                    {
+                        breakpoint: 767,
+                        settings: {
+                            vertical: false,
+                        }
+                    }
+                ]
+            });
         } else {
-            // console.log('masuk')
+            $sliderDiscovery.slick({
+                dots: false,
+                infinite: false,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                rows: 2,
+                draggable: false,
+                arrows: true,
+                autoplay: false,
+                vertical: true,
+                appendArrows: $(this).find('.arrows'),
+                responsive: [
+                    {
+                        breakpoint: 767,
+                        settings: {
+                            vertical: false,
+                        }
+                    }
+                ]
+            });
             // for (var i = 0; i < $sliderDiscoveryItem.length; i += 6) {
             //     $sliderDiscoveryItem.slice(i, i + 4).wrapAll(`<div class="container-card"></div>`);
             // }
         }
-        
+
     });
 });
